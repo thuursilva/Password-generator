5f935f71d35a7cdd68e5008ad414299b50756c82
Arthur Fernando <55673628+thuursilva@users.noreply.github.com>
2023-08-28 11:47:45 -0300

    Definindo estilo da página e variáveis

---
+++ main.js
@@ -0,0 +1,15 @@
+//constantes
+const length = document.querySelector(".length input");
+const options = document.querySelector(".lista input");
+const copyIcon = document.querySelector(".copia_senha");
+const senha = document.querySelector("#senha");
+const indicador = document.querySelector(".indicador");
+const generate = document.querySelector(".btn_generator");
+
+const caracteres = {
+    lowercase = "abcdefghijklmnopqrstuvwxyz",
+    uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+    numbers = "0123456789",
+    symbols = "!@#$%&*()[]{}:;.,~-_<>+"
+};
+
